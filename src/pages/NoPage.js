2fa7859c9882e893d7fae7652e3e35ca7fb24946
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-08-30 20:12:37 -0400

    e

---
+++ src/pages/NoPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NoPage() {
+  return <h1 className="text-5xl text-white">404</h1>;
+}
--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import Bell from "../assets/bellschedule2122B.pdf";
+import Courses from "../assets/CourseCatalog2022-2023.pdf";
+
 export default function Resources() {
   return (
     <>
@@ -11,6 +14,110 @@ export default function Resources() {
         All resources, including notes, homework, and study guides are posted
         here
       </p>
+      <h3 className="mt-5 ml-10 text-3xl underline subhead">Useful Links</h3>
+      <ul className="mt-5 ml-10 text">
+        <li className="ml-4">
+          <a
+            href="https://www.millburn.org/"
+            className="underline alert hvr-grow"
+            target="_blank"
+          >
+            ○ Official MHS website
+          </a>
+        </li>
+
+        <li className="ml-4">
+          <a
+            href="https://drive.google.com/drive/folders/0BytHEdOU5suiZU1ZR2w4dDQ2czg?resourcekey=0-2rnog7nwl51dsap7gxGPIQ"
+            className="underline alert hvr-grow"
+            target="_blank"
+          >
+            ○ Future MHS Calendars
+          </a>
+        </li>
+
+        <li className="ml-4">
+          <a
+            href="https://millburn.powerschool.com/public/"
+            className="underline alert hvr-grow"
+            target="_blank"
+          >
+            ○ Powerschool
+          </a>
+        </li>
+
+        <li className="ml-4">
+          <a href={Bell} className="underline alert hvr-grow" target="_blank">
+            ○ Bell Schedules
+          </a>
+        </li>
+
+        <li className="ml-4">
+          <a
+            href={Courses}
+            className="underline alert hvr-grow"
+            target="_blank"
+          >
+            ○ Course Catalog 22-23
+          </a>
+        </li>
+
+        <li className="ml-4">
+          <a
+            href="https://launchpad.classlink.com/millburn"
+            className="underline alert hvr-grow"
+            target="_blank"
+          >
+            ○ Millburn Classlink
+          </a>
+        </li>
+
+        <li className="ml-4">
+          <a
+            href="https://mhs.millburn.org/apps/pages/index.jsp?uREC_ID=811686&type=d&pREC_ID=1324940"
+            className="underline alert hvr-grow"
+            target="_blank"
+          >
+            ○ MHS Calendars (more detailed)
+          </a>
+        </li>
+        <li className="ml-4">
+          <a
+            href="https://sci-hub.se/"
+            className="underline alert hvr-grow"
+            target="_blank"
+          >
+            ○ SciHub - Pirate science articles/journals
+          </a>
+        </li>
+        <li className="ml-4">
+          <a
+            href="https://libgen.is/"
+            className="underline alert hvr-grow"
+            target="_blank"
+          >
+            ○ Library Genesis - Pirate books
+          </a>
+        </li>
+        <li className="ml-4">
+          <a
+            href="https://z-lib.org/"
+            className="underline alert hvr-grow"
+            target="_blank"
+          >
+            ○ Z-lib - Pirate books
+          </a>
+        </li>
+        <li className="ml-4">
+          <a
+            href="https://www.wolframalpha.com/"
+            className="underline alert hvr-grow"
+            target="_blank"
+          >
+            ○ WoframAlpha - Online calculator for higher-level maths
+          </a>
+        </li>
+      </ul>
     </>
   );
 }
